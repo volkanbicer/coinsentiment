ad3e11d4866d8192fad676dde0258f6d795e16d1
vlknbcr <vlknbcr@gmail.com>
2018-03-21 13:38:13 +0300

    Configs changed

--- appConfig.py
+++ appConfig.py
@@ -1,6 +1,7 @@
+import os
 
-reddit = {'client_id': '****8',
-          'client_secret': '*****',
-          'password':'*****',
-          'user_agent':'******',
-          'username':'******'}
+reddit = {'client_id': os.environ['client_id'],
+          'client_secret': os.environ['client_secret'],
+          'password':os.environ['password'],
+          'user_agent':os.environ['user_agent'],
+          'username':os.environ['username']}
